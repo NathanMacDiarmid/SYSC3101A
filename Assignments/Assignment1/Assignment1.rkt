f3cd6c806b5bf4198e3cd50d9f558c85a62bcb93
Nathan MacDiarmid <nathanmacdiarmid@cmail.carleton.ca>
2023-02-28 12:21:28 -0500

    Finished assignment 1 and lab3

--- Assignments/Assignment1/Assignment1.rkt
+++ Assignments/Assignment1/Assignment1.rkt
@@ -8,9 +8,13 @@
 
 (define (count-multiples lst n)
   (cond
+    ; Verifies the list is not null
     [(empty? lst) + 0]
+    ; Verifies that n is greater than 1 as per requirements
     [(< n 1) + 0]
+    ; Adds one to the total sum if a multiple is found
     [(= (modulo (car lst) n) 0) (+ 1 (count-multiples (cdr lst) n))]
+    ; Goes to next element in list
     [(count-multiples (cdr lst) n)]
    )
   )
@@ -18,9 +22,11 @@
 ; Question 2
 
 (define (count-multiples-iter lst n)
+  ; for/sum adds the result of all the iterations
   (for/sum ([i lst])
     (cond
       [(< n 1) 0]
+      ; Verifies that the element in the list is a multiple of n
       [(= (modulo i n) 0) 1]
       [0]
       )
